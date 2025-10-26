d1b9eff042a033162449fafd60886c1ea263786a
burtonjong <burtonjong05@gmail.com>
2025-10-26 01:15:53 -0600

    update: modify multiply functionality

--- multiply.py
+++ multiply.py
@@ -6,7 +6,6 @@ def multiply(a, b):
     iterations = abs(b)
     
     for _ in range(iterations):
-        result = add(result, a)
+        result = add(result, abs(a))
     
     return -result if is_negative else result
-
